956af9ec10bcac13ca38edef0d4db4190e0ecc58
jhembe <jhembe202@gmail.com>
2022-05-04 11:41:39 +0300

    This is the second commit from the fastapi todo app

--- models.py
+++ models.py
@@ -7,3 +7,11 @@ class Todo(Base):
     id = Column(Integer,primary_key=True,index=True)
     title = Column(String)
     complete = Column(Boolean,default=False)
+    
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer,primary_key=True,index=True)
+    fname = Column(String)
+    lname = Column(String)
+    email = Column(String)
